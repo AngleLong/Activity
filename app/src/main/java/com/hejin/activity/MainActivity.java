9c26c85f1688564c8c85dc36b72b9cb5d70af92a
hejinlong <hejinlong@haixun360.com>
2017-10-13 15:50:28 +0800

    Initial commit

---
+++ app/src/main/java/com/hejin/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hejin.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 关于Activity的一些应用
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
